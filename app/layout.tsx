4f8846be32c6bde5497c7327931be31743380a79
Simon Grigenas <grigenas@outlook.com>
2023-05-13 14:41:04 -0700

    Fixed Homepage product tiles bug for carousel and three-tile components

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ const { TWITTER_CREATOR, TWITTER_SITE, SITE_NAME } = process.env;
 
 export const metadata = {
   title: {
-    default: SITE_NAME,
+    default: "Snowboard Shop",
     template: `%s | ${SITE_NAME}`
   },
   robots: {
@@ -33,13 +33,13 @@ const inter = Inter({
 export default async function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" className={inter.variable}>
-      <body className="bg-white text-black selection:bg-teal-300 dark:bg-black dark:text-white dark:selection:bg-fuchsia-600 dark:selection:text-white">
-        {/* @ts-expect-error Server Component */}
-        <Navbar />
-        <Suspense>
-          <main>{children}</main>
-        </Suspense>
-      </body>
+        <body className="bg-white text-black selection:bg-teal-300 dark:bg-black dark:text-white dark:selection:bg-fuchsia-600 dark:selection:text-white">
+          {/* @ts-expect-error Server Component */}
+            <Navbar />
+            <Suspense>
+              <main>{children}</main>
+            </Suspense>
+        </body>
     </html>
   );
 }
--- components/carousel.tsx
+++ components/carousel.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 
 export async function Carousel() {
   // Collections that start with `hidden-*` are hidden from the search page.
-  const products = await getCollectionProducts({ collection: 'hidden-homepage-carousel' });
+  const products = await getCollectionProducts({ collection: 'Hydrogen' });
 
   if (!products?.length) return null;
 
